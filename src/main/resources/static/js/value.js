bd468b5e79517014e4e9dd7d0aa9fb3361606a22
dsomasundar2 <dsomasundar2@CSCINDAI408872.asiapac.globalcsc.net>
2017-03-11 17:16:36 +0530

    ur up

--- src/main/resources/static/js/value.js
+++ src/main/resources/static/js/value.js
@@ -1,5 +1,5 @@
    app.value("subTitle","Topics Library");
-   app.value("url","https://springbootangular.herokuapp.com/");   
+   app.value("url","https://springbootangular.herokuapp.com");   
    app.value("HeaderValues",{
    "th1":"Id",
    "th2":"Name",
